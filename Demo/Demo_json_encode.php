187753d43fe2f1cd07c22bb1246065d867adf4b1
alanwang1207 <alan_wang@mail.chungyo.net>
2020-08-20 14:21:17 +0800

    修改productbyid

--- Demo/Demo_json_encode.php
+++ Demo/Demo_json_encode.php
@@ -5,20 +5,20 @@ header("content-type:text/html; charset=utf-8");
 // 0. 請先建立 Class 資料庫 （執行 class.sql）
 
 // 1. 連接資料庫伺服器
-$link = mysql_connect("127.0.0.1", "root", 'Pa$$w0rd') or die(mysql_error());
+$link = mysqli_connect("127.0.0.1", "root", 'root') or die(mysqli_connect_error());
 
-$result = mysql_query("set names utf8", $link);
-mysql_select_db("class", $link);
+$result = mysqli_query("set names utf8", $link);
+mysqli_select_db("class", $link);
 
 // 2. 執行 SQL 敘述
 $commandText = "select * from students";
-$result = mysql_query($commandText, $link);
+$result = mysqli_query($commandText, $link);
 
 // $row = mysql_fetch_assoc($result);
 // $row = mysql_fetch_row($result);
 // $row = mysql_fetch_array($result);
 
-$row = mysql_fetch_assoc($result);
+$row = mysqli_fetch_assoc($result);
 
 $json = json_encode($row);
 
---
+++ Lab_REST_API/api/api.php
@@ -0,0 +1,59 @@
+<?php
+// 一個簡單但可以運作的 REST API，
+// 物件導向與MVC課程時，再來寫進化版
+
+$method = $_SERVER['REQUEST_METHOD'];
+$url = explode("/", rtrim($_GET["url"], "/"));
+//資料庫連線
+$dbLink = @mysqli_connect("localhost", "root", "root") or die(mysqli_connect_error());
+mysqli_query($dbLink, "set names utf8");
+mysqli_select_db($dbLink, "apiDB");
+
+switch ($method . " " . $url[0]) {
+    case "POST products":
+        echo "creating ...";
+        break;
+    case "GET products":
+        if (isset($url[1]))
+            getProductById($url[1]);
+        else
+            getProducts();
+        break;
+    case "PUT products":
+        echo 'update ...';
+        break;
+    case "DELETE products":
+        echo 'delete ...';
+        break;
+    default:
+        echo "Thank you";
+}
+mysqli_close($dbLink);
+
+
+function getProductById($id)
+{
+    if (!is_numeric($id))
+        die("id is not a number.");
+
+    global $dbLink;
+    $result = mysqli_query(
+        $dbLink,
+        "select * from products where productId = " . $id
+    );
+    $row = mysqli_fetch_assoc($result);
+    echo json_encode($row);
+}
+
+function getProducts()
+{
+    global $dbLink;
+    $result = mysqli_query(
+        $dbLink,
+        "select * from products"
+    );
+    while ($row = mysqli_fetch_assoc($result)) {
+        $datalist[] = $row;
+    }
+    echo json_encode($datalist);
+}
